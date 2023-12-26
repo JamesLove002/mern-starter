041fa49cac16cb55533b788ffbee06172eb453fd
Cody Love <cody.love002@gmail.com>
2023-12-26 14:17:27 +1100

    server folder structure and jest configured and functioning

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['server'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'server/tsconfig.json',
+    },
+  },
+};
---
+++ server/src/server.test.ts
@@ -0,0 +1,3 @@
+test("tests can run", () => {
+  expect(1 + 2).toBe(3);
+});
